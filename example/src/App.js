65f8b7ecf113ad169059e6307b51f0d0539e2532
ccmikechen <ccmikechen@gmail.com>
2019-02-27 18:33:58 +0800

    Add example

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'react-upgrade-block'
+import ReactUpgradeBlock from 'react-upgrade-block'
+import Image from './images/react.png'
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { value: 0 }
+    this.maxValue = 10
+  }
+
+  handleClick = () => {
+    const { value } = this.state
+    if (value >= this.maxValue) {
+      return
+    }
+    this.setState({ value: this.state.value + 1 })
+  }
+
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ReactUpgradeBlock
+          image={Image}
+          size={70}
+          borderRadius={10}
+          borderColor="#CCDDEE"
+          borderWidth={2}
+          buttonSize={20}
+          buttonShift={5}
+          buttonBorderColor="#FF5555"
+          buttonColor="#EE5555"
+          value={this.state.value}
+          maxValue={this.maxValue}
+          fontSize={16}
+          textColor="#FF0000"
+          textShift={2}
+          onClick={this.handleClick} />
       </div>
     )
   }
